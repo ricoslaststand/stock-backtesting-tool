28e03dd749772d52b834ec187b5f955bc5309ecb
Connor Young <connoryoungkicker@gmail.com>
2024-02-11 19:43:21 -0500

    Reformatted files

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
@@ -12,4 +13,3 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
-
