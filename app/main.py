6bd0611de8202f9b84d232cfce30a5c8071642c6
Connor Young <connoryoungkicker@gmail.com>
2024-02-13 22:06:53 -0500

    Remove newline

--- app/main.py
+++ app/main.py
@@ -1,10 +1,30 @@
 from fastapi import FastAPI
 
-from app.strategies import AvgComparedToXDayAvgStrategy
+import yfinance as yf
+import backtrader as bt
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": AvgComparedToXDayAvgStrategy.__name__}
+    cerebro = bt.Cerebro()
+    # cerebro.addstrategy(AvgComparedToXDayAvgStrategy)
+
+    # Add a strategy
+    cerebro.addstrategy(bt.Strategy)
+
+    cerebro.broker.setcash(100000)
+
+    # fileName = "YHOO.csv"
+    # file = open(fileName, "w")
+    # file.close()
+
+    # Create a Data Feed
+    data = bt.feeds.PandasData(
+        dataname=yf.download("SPY", "2021-06-06", "2021-07-01", auto_adjust=True)
+    )
+    # Add the Data Feed to Cerebro
+    cerebro.adddata(data)
+    cerebro.run()
+    return {"Hello": cerebro.broker.getvalue()}
--- app/strategies/AvgComparedToXDayAvgStrategy.py
+++ app/strategies/AvgComparedToXDayAvgStrategy.py
@@ -5,4 +5,13 @@ class AvgComparedToXDayAvgStrategy(bt.Strategy):
     name = "AvgComparedToXDayAvgStrategy"
 
     def __init__(self):
-        self.data = [0]
+        self.sma = bt.SimpleMovingAverage(period=15)
+
+    def next(self):
+        if self.sma > self.data.close:
+            # Do something
+            pass
+
+        elif self.sma < self.data.close:
+            # Do something else
+            pass
