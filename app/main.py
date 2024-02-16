ad330d3b1cd669a21b97755505bd020a49545872
Connor Young <connoryoungkicker@gmail.com>
2024-02-16 00:16:04 -0500

    Formatted code

--- app/main.py
+++ app/main.py
@@ -4,6 +4,8 @@ import yfinance as yf
 
 import json
 
+import requests
+
 from pandas_datareader import data as pdr
 
 # download dataframe
@@ -30,3 +32,25 @@ def read_root(ticker_symbol: str = "SPY"):
     print("results =", results)
 
     return json.loads(results.to_json())
+
+
+@app.get("/stocks")
+def read_stocks():
+    response = requests.get(
+        "https://pkgstore.datahub.io/core/s-and-p-500-companies/constituents_json/data/297344d8dc0a9d86b8d107449c851cc8/constituents_json.json"
+    )
+
+    if not response.ok:
+        return {"error": "Unable to fetch stocks data"}
+
+    return response.json()
+
+
+@app.get("/strategies")
+def read_strategies():
+    arr = []
+
+    for strategy in [VolumeDiff]:
+        arr.append({"name": strategy.name})
+
+    return {"strategies": arr}
